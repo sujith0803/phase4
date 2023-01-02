3f9ddbfb8002a77eb5e96d5f122f9e2f040702de
sujith0803 <sujithpujari2000@gmail.com>
2023-01-02 18:10:06 +0530

    Angular

---
+++ css/typescript/demo5.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var Demo4_1 = require("./Demo4");
+var D = /** @class */ (function () {
+    function D() {
+        console.log("Hi D constructor ");
+    }
+    return D;
+}());
+var d = new D();
+var a = new Demo4_1.A();
---
+++ css/typescript/demo5.ts
@@ -0,0 +1,2 @@
+import { A } from './Demo4'
+class D{constructor(){console.log("Hi D constructor ")}}var d= new D();var a= new A();
